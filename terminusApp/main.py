4be850b70e7c76bbe715c39b4abe522e2dfab51f
Adam Pawelek <adam.pwk@outlook.com>
2023-02-28 01:08:04 +0100

    cleaned main

--- terminusApp/main.py
+++ terminusApp/main.py
@@ -33,17 +33,3 @@ app.include_router(auth.routerr.router)
 
 
 
-########################
-
-'''
-@terminusApp.post("/add-player")
-async def say_hello(item :Item):
-    my_dog = Pet(name=item.name, species=item.species, age=item.age, weight=item.weight)
-    my_player = Player(name='kkkkk',age=10,weight= 10.0,pets = [my_dog] )
-    client.insert_document([my_player])
-
-
-    return {"message": f"Hello {item}"}
-
-'''
-
